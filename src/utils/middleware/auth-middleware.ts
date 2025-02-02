a2258fca0aab7b2f7548d58e39d5b21a70f62e25
jasrod12ucsm <jason.rodriguez@ucsm.edu.pe>
2025-02-02 16:55:28 -0500

    return-type

--- src/utils/middleware/auth-middleware.ts
+++ src/utils/middleware/auth-middleware.ts
@@ -1,10 +1,38 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { JwtService } from '@nestjs/jwt';
+interface JwtPayload {
+  name: string;
+  id: string;
+}
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly jwtService: JwtService) {}
+
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
-    next();
+    console.log('AuthMiddleware');
+
+    // Extracción del token Bearer
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send('No token provided');
+    }
+
+    const tokenParts = authHeader.split(' ');
+    if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+      return res.status(401).send('Invalid token format');
+    }
+
+    const token: string = tokenParts[1];
+    try {
+      // Verificar token
+      const decoded = this.jwtService.verify<JwtPayload>(token);
+      req['data'] = decoded; // Puedes agregar el usuario decodificado al objeto de solicitud
+      next();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      return res.status(401).send('Invalid token');
+    }
   }
 }
